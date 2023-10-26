3f7b11fbb8ca8bb2a267e0b8d5016ca579c03994
Gabriele-Zini <gabrielezini845@gmail.com>
2023-10-26 19:59:32 +0200

    add: odd/even exercise

---
+++ pari e dispari/README.md
@@ -0,0 +1,20 @@
+## ESERCIZIO
+
+**Pari e Dispari**
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto.
+
+### SOLUZIONE
+
+**raccolta dati**
+
+1. scelta dell'utente su pari o dispari
+2. numero inserito dall'utente da 1 a 5
+
+**logica del programma**
+
+1. generiamo un numero random da 1 a 5 per il computer usando una funzione con il metodo Math.random()
+2. creiamo una funzione che, data la somma tra due numeri, determini se la somma è pari o dispari;
---
+++ pari e dispari/js/script.js
@@ -0,0 +1,92 @@
+const eventBtn = document.getElementById("even-btn");
+const oddBtn = document.getElementById("odd-btn");
+const numberUser = document.getElementById("user-number");
+let numberUserSelect;
+let randomNum;
+const computerNumber = document.getElementById("rnd-number");
+let userChoice;
+let result = document.querySelector(".result");
+let message = "";
+
+// event listener per il numero selezionato dall'utente
+numberUser.addEventListener("change", function () {
+  numberUserSelect = parseInt(numberUser.value);
+  console.log(numberUserSelect + " numero utente");
+});
+
+// bottone per scegliere dispari
+eventBtn.addEventListener("click", function () {
+  userChoice = "even";
+  console.log("l'utente ha scelto pari");
+  eventBtn.classList.add("bg-red")
+  oddBtn.classList.remove("bg-red")
+});
+
+// bottone per scegliere pari
+oddBtn.addEventListener("click", function () {
+  userChoice = "odd";
+  console.log("l'utente ha scelto dispari");
+  oddBtn.classList.add("bg-red")
+  eventBtn.classList.remove("bg-red")
+});
+
+// bottone per generare un numero random del computer
+computerNumber.addEventListener("click", function () {
+  randomNumber();
+  checkResult();
+  console.log("---------------------------");
+  result.innerHTML = message;
+});
+
+// funzione per generare il numero random
+function randomNumber() {
+  randomNum = Math.floor(Math.random() * 5 + 1);
+  console.log(randomNum + " numero computer");
+}
+
+// funzione per sapere se la somma dei due numeri è pari o dispari
+function odd_Or_Even_Sum(number1, number2) {
+  let sum = number1 + number2;
+  let flag = false;
+  if (sum % 2 === 0) {
+    flag = true;
+  }
+  return flag;
+}
+
+// funzione che stampa il risultato
+function checkResult() {
+  let sum = randomNum + numberUserSelect;
+  message = "";
+
+  if (userChoice == "even" && odd_Or_Even_Sum(randomNum, numberUserSelect)) {
+    console.log(
+      `${randomNum} + ${numberUserSelect} = ${sum} è pari, l'utente ha vinto`
+    );
+    message = `${randomNum} (numero dell'utente) + ${numberUserSelect} (numero del computer) = ${sum} è pari, l'utente ha <span class="result-span">vinto</span>`;
+  } else if (
+    userChoice === "odd" &&
+    !odd_Or_Even_Sum(randomNum, numberUserSelect)
+  ) {
+    console.log(
+      `${randomNum} + ${numberUserSelect} = ${sum} è dispari, l'utente ha <span class="result-span">vinto</span>`
+    );
+    message = `${randomNum} (numero dell'utente) + ${numberUserSelect} (numero del computer) = ${sum} è dispari, l'utente ha <span class="result-span">vinto</span>`;
+  } else if (
+    userChoice == "even" &&
+    !odd_Or_Even_Sum(randomNum, numberUserSelect)
+  ) {
+    console.log(
+      `${randomNum} + ${numberUserSelect} = ${sum} è dispari, l'utente ha perso`
+    );
+    message = `${randomNum} (numero dell'utente) + ${numberUserSelect} (numero del computer) = ${sum} è dispari, l'utente ha <span class="result-span">perso</span>`;
+  } else if (
+    userChoice === "odd" &&
+    odd_Or_Even_Sum(randomNum, numberUserSelect)
+  ) {
+    console.log(
+      `${randomNum} + ${numberUserSelect} = ${sum} è pari, l'utente ha perso`
+    );
+    message = `${randomNum} (numero dell'utente) + ${numberUserSelect} (numero del computer) = ${sum} è pari, l'utente ha <span class="result-span">perso</span>`;
+  }
+}
